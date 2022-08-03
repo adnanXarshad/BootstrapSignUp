2ef79c53ca06c6240145f365b8831300648302b6
Adnan Arshad <adnan.arshad4585@gmail.com>
2022-08-03 16:24:29 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center mt-5 d-flex justify-content-center">
+      <form style={{ width: 350 }}>
+        <img
+          className="mb-3"
+          src="Logo.svg"
+          height={"16%"}
+          width={"16%"}
+          alt="logo"
+        />
+        <h1 className="mb-3 h3 font weight normal">Sign In Page</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Build a sign-in page to display your command on bootstrap{" "}
+          <span style={{ color: "red" }}>text with color red</span> pseudo
+          element.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href={"https://www.google.com/"}>
+          Works in latest Chrome, Safari and Firefox. This is a clickable
+          section that will take us to new page.
         </a>
-      </header>
+
+        <div className={"mt-3 mb-3"}>
+          <input
+            type={"text"}
+            className={"form-control mb-2"}
+            placeholder={"username"}
+            required
+            autoFocus
+          />
+          <input
+            type={"password"}
+            className={"form-control"}
+            placeholder={"password"}
+          />
+        </div>
+        <div className={"checkbox"}>
+          <input type={"checkbox"} value={"Remember Me"} /> Remember Me
+        </div>
+        <div className={"mt-3 d-grid gap-6 col-12 mx-auto"}>
+          <button className={"btn btn-lg btn-primary btn-block"}>
+            Sign In
+          </button>
+        </div>
+        <div className="mt-4" style={{ color: "grey" }}>
+          <small>
+            <em>2017-2018</em>
+          </small>
+        </div>
+      </form>
     </div>
   );
 }
